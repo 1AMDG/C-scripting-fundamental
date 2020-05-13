7ffecdb28510f67049e39fb8c18766eeb3509191
1AMDG <58343206+1AMDG@users.noreply.github.com>
2020-05-13 18:07:58 -0400

    GameObject itnneraction and plaer control
    yes

---
+++ Assets/Scripts/CameraRotator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotator : MonoBehaviour
+{
+    float cameraSpeed = 200f;
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (Input.GetAxis("Mouse X") > 0)
+        {
+            transform.Rotate(0, cameraSpeed * Time.deltaTime, 0);
+        }
+        else if (Input.GetAxis("Mouse X") < 0)
+        {
+            transform.Rotate(0, -cameraSpeed * Time.deltaTime, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public CapsuleCollider playerCollider;
+    public float moveSpeed = 5f;
+    //private GameObject enemy;
+    // private GameObject enemyScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerCollider = this.GetComponent<CapsuleCollider>();
+        playerCollider.height = 1.0f;
+        playerCollider.center = new Vector3(0f, 0.5f, 0f);
+
+        //enemy = GameObject.Find("Battle_Dummy");
+       // enemyScript = enemy.GetComponent<Enemy>();  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        
+        Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical);
+
+        transform.Translate(movement * Time.deltaTime * moveSpeed);
+
+      
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "enemy")
+        {
+            // enemyScript = collision.gameObject.GetComponent<Enemy>();
+            // enemyScript.enemyHealth--;
+        }
+    }
+}
